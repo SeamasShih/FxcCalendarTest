d7ad3ab12b4137856e712c2cfcd9b79c1af5384c
SeamasShih <seamas0716@gmail.com>
2018-11-13 14:18:57 +0800

    Complete add event function

--- app/src/main/java/com/honhai/foxconn/fxccalendar/activities/AddEventActivity.java
+++ app/src/main/java/com/honhai/foxconn/fxccalendar/activities/AddEventActivity.java
@@ -1,30 +1,36 @@
 package com.honhai.foxconn.fxccalendar.activities;
 
 import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.honhai.foxconn.fxccalendar.R;
 import com.honhai.foxconn.fxccalendar.data.Data;
+import com.honhai.foxconn.fxccalendar.data.Event;
 import com.honhai.foxconn.fxccalendar.views.add.Toolbar;
+import com.honhai.foxconn.fxccalendar.views.add.item.color.ColorItem;
 
 public class AddEventActivity extends AppCompatActivity {
 
-    int year, month, weekOfMonth, dayOfMonth, hour, minute;
-    TextView startWMDY, startHM;
-    TextView endWMDY, endHM;
-    EditText editText;
-    Toolbar toolbar;
+    private TextView startWMDY, startHM;
+    private TextView endWMDY, endHM;
+    private EditText editText;
+    private Toolbar toolbar;
+    private ColorItem colorItem;
+    private int prevColor;
+    private int currColor;
+    private int[] startTime = new int[6];
+    private int[] endTime = new int[6];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +38,10 @@ public class AddEventActivity extends AppCompatActivity {
         Intent intent = getIntent();
         int[] dayInfo = intent.getIntArrayExtra("dayInfo");
         if (dayInfo != null) {
-            year = dayInfo[0];
-            month = dayInfo[1];
-            weekOfMonth = dayInfo[2];
-            dayOfMonth = dayInfo[3];
-            hour = dayInfo[4];
-            minute = dayInfo[5];
+            for (int i = 0; i < dayInfo.length; i++) {
+                startTime[i] = dayInfo[i];
+                endTime[i] = dayInfo[i];
+            }
         }
         setContentView(R.layout.activity_add_event);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
@@ -47,25 +51,130 @@ public class AddEventActivity extends AppCompatActivity {
         setDayText();
         setToolbar();
         setColor();
+        setTimeSettingListener();
+    }
+
+    private void setTimeSettingListener() {
+        startWMDY.setOnClickListener(v -> {
+                    DatePickerDialog dp = new DatePickerDialog(
+                            this,
+                            (view, year, monthOfYear, dayOfMonth) -> {
+                                startTime[0] = year;
+                                startTime[1] = monthOfYear;
+                                startTime[3] = dayOfMonth;
+                                setDayText();
+                            },
+                            startTime[0], startTime[1], startTime[3]);
+                    dp.show();
+                }
+        );
+        startHM.setOnClickListener(v -> {
+                    TimePickerDialog dp = new TimePickerDialog(
+                            this,
+                            (view, hourOfDay, minute) -> {
+                                startTime[4] = hourOfDay;
+                                startTime[5] = minute;
+                                setDayText();
+                            },
+                            startTime[4], startTime[5], true);
+                    dp.show();
+                }
+        );
+        endWMDY.setOnClickListener(v -> {
+                    DatePickerDialog dp = new DatePickerDialog(
+                            this,
+                            (view, year, monthOfYear, dayOfMonth) -> {
+                                endTime[0] = year;
+                                endTime[1] = monthOfYear;
+                                endTime[3] = dayOfMonth;
+                                setDayText();
+                            },
+                            endTime[0], endTime[1], endTime[3]);
+                    dp.show();
+                }
+        );
+        endHM.setOnClickListener(v -> {
+                    TimePickerDialog dp = new TimePickerDialog(
+                            this,
+                            (view, hourOfDay, minute) -> {
+                                endTime[4] = hourOfDay;
+                                endTime[5] = minute;
+                                setDayText();
+                            },
+                            endTime[4], endTime[5], true);
+                    dp.show();
+                }
+        );
     }
 
     @SuppressLint("NewApi")
     private void setToolbar() {
         toolbar.inflateMenu(R.menu.add_event_toolbar_widget);
+        toolbar.setOnMenuItemClickListener(menuItem -> {
+            if (menuItem.getItemId() == R.id.done) {
+                if (editText.getText().length() == 0)
+                    Toast.makeText(this, "Event name must not be empty!", Toast.LENGTH_SHORT).show();
+                else if (!timeIsAvailable())
+                    Toast.makeText(this, "Time setting is error!", Toast.LENGTH_SHORT).show();
+                else {
+                    Data.getInstance().events.add(new Event(
+                            editText.getText().toString(), startTime[0], startTime[1], startTime[3], startTime[4], startTime[5],
+                            endTime[0], endTime[1], endTime[3], endTime[4], endTime[5], currColor
+                    ));
+                    finish();
+                }
+            }
+            return true;
+        });
+    }
+
+    private boolean timeIsAvailable() {
+        for (int i = 0; i < startTime.length; i++) {
+            if (i == 2) continue;
+            if (startTime[i] < endTime[i])
+                return true;
+            else if (startTime[i] > endTime[i])
+                return false;
+            else if (i == startTime.length - 1)
+                return false;
+        }
+        return true;
     }
 
     private void setColor() {
-        int color = Data.getInstance().themeColor;
-        editText.setHintTextColor(Color.argb(100, Color.red(color), Color.green(color), Color.blue(color)));
-        editText.setTextColor(color);
-        toolbar.getMenu().getItem(0).setIconTintList(ColorStateList.valueOf(color));
+        setColor(Data.getInstance().themeColor);
+        prevColor = Data.getInstance().themeColor;
     }
 
+    public void setColor(int color) {
+        editText.setHintTextColor(Color.argb(100, Color.red(color), Color.green(color), Color.blue(color)));
+        editText.setTextColor(color);
+        Drawable icon = toolbar.getMenu().getItem(0).getIcon();
+        icon.setTintList(ColorStateList.valueOf(color));
+        toolbar.getMenu().getItem(0).setIcon(icon);
+        startWMDY.setTextColor(color);
+        startHM.setTextColor(color);
+        endWMDY.setTextColor(color);
+        endHM.setTextColor(color);
+        colorItem.setColor(color);
+        currColor = color;
+    }
+
+    public void refreshPrevColor() {
+        prevColor = currColor;
+    }
+
+    public void recoverColor() {
+        currColor = prevColor;
+        setColor(currColor);
+    }
+
+    @SuppressLint("SetTextI18n")
     private void setDayText() {
-        startWMDY.setText(year + " " + month + "/" + dayOfMonth);
-        startHM.setText(hour + " : " + minute);
-        endWMDY.setText(year + " " + month + "/" + dayOfMonth);
-        endHM.setText(hour + " : " + minute);
+        startWMDY.setText(startTime[0] + " " + (startTime[1]+1 > 9 ? startTime[1]+1 : "0" + startTime[1]+1) + "/" + (startTime[3] > 9 ? startTime[3] : "0" + startTime[3]));
+        startHM.setText((startTime[4] > 9 ? startTime[4] : "0" + startTime[4]) + " : " + (startTime[5] > 9 ? startTime[5] : "0" + startTime[5]));
+        endWMDY.setText(endTime[0] + " " + (endTime[1]+1 > 9 ? endTime[1]+1 : "0" + endTime[1]+1) + "/" + (endTime[3] > 9 ? endTime[3] : "0" + endTime[3]));
+        endHM.setText((endTime[4] > 9 ? endTime[4] : "0" + endTime[4]) + " : " + (endTime[5] > 9 ? endTime[5] : "0" + endTime[5]));
     }
 
     private void findViews() {
@@ -75,5 +184,6 @@ public class AddEventActivity extends AppCompatActivity {
         endHM = findViewById(R.id.endHM);
         editText = findViewById(R.id.edit);
         toolbar = findViewById(R.id.tool);
+        colorItem = findViewById(R.id.colorItem);
     }
 }
--- app/src/main/java/com/honhai/foxconn/fxccalendar/views/add/item/color/ColorItem.java
+++ app/src/main/java/com/honhai/foxconn/fxccalendar/views/add/item/color/ColorItem.java
@@ -1,6 +1,7 @@
 package com.honhai.foxconn.fxccalendar.views.add.item.color;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.OrientationHelper;
@@ -10,9 +11,12 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import com.honhai.foxconn.fxccalendar.R;
+import com.honhai.foxconn.fxccalendar.activities.AddEventActivity;
 import com.honhai.foxconn.fxccalendar.views.add.item.SampleView;
 import com.honhai.foxconn.fxccalendar.views.add.popup.color.ColorPopupWindow;
 import com.honhai.foxconn.fxccalendar.views.add.popup.widget.ColorChoiceDot;
@@ -23,6 +27,10 @@ import java.util.List;
 public class ColorItem extends SampleView {
 
     private Context context;
+    private ImageView imageView;
+    private TextView textView;
+    private ImageView indicator;
+    private boolean isConfirm = false;
 
     public ColorItem(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -31,21 +39,40 @@ public class ColorItem extends SampleView {
     }
 
     private void showPopupWindow() {
+        isConfirm = false;
         View view = LayoutInflater.from(context).inflate(R.layout.add_event_color_popup_window, null);
         RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(context, OrientationHelper.VERTICAL, false));
         recyclerView.setAdapter(new Adapter(recyclerView));
+        TextView confirm = view.findViewById(R.id.confirm);
         ColorPopupWindow popupWindow = new ColorPopupWindow(view);
         popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+        confirm.setOnClickListener(v -> {
+            isConfirm = true;
+            popupWindow.dismiss();
+        });
+        popupWindow.setOnDismissListener(() -> {
+            if (isConfirm)
+                ((AddEventActivity) context).refreshPrevColor();
+            else
+                ((AddEventActivity) context).recoverColor();
+        });
     }
 
     @Override
     public void setColor(int color) {
-
+        if (imageView == null)
+            findViews();
+        imageView.setBackgroundTintList(ColorStateList.valueOf(color));
+        textView.setTextColor(color);
+        indicator.setBackgroundTintList(ColorStateList.valueOf(color));
     }
 
     @Override
     public void findViews() {
+        imageView = findViewById(R.id.colorImage);
+        textView = findViewById(R.id.colorText);
+        indicator = findViewById(R.id.colorIndicator);
     }
 
     private class Adapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
@@ -71,19 +98,21 @@ public class ColorItem extends SampleView {
             ColorPreviewBar bar = ((ViewHolder) viewHolder).bar;
             TextView textView = ((ViewHolder) viewHolder).textView;
             ColorChoiceDot dot = ((ViewHolder) viewHolder).dot;
+            View parent = ((ViewHolder) viewHolder).parent;
 
             bar.setColor(colors[position]);
             textView.setTextColor(colors[position]);
             dot.setColor(colors[position]);
             dot.setChosen(checked == position);
-            dot.setOnClickListener(v -> {
+            parent.setOnClickListener(v -> {
                 if (checked != position) {
                     View view = recyclerView.getLayoutManager().findViewByPosition(checked);
-                    if (view != null){
-                        ((ColorChoiceDot)view.findViewById(R.id.choice)).setChosen(false);
+                    if (view != null) {
+                        ((ColorChoiceDot) view.findViewById(R.id.choice)).setChosen(false);
                     }
                     checked = position;
-                    ((ColorChoiceDot)v).setChosen(true);
+                    dot.setChosen(true);
+                    ((AddEventActivity) context).setColor(colors[position]);
                 }
             });
         }
@@ -104,12 +133,14 @@ public class ColorItem extends SampleView {
             private ColorPreviewBar bar;
             private TextView textView;
             private ColorChoiceDot dot;
+            private View parent;
 
             private ViewHolder(@NonNull View itemView) {
                 super(itemView);
                 bar = itemView.findViewById(R.id.color);
                 textView = itemView.findViewById(R.id.label);
                 dot = itemView.findViewById(R.id.choice);
+                parent = itemView;
             }
         }
     }
